6b773efe3adbc390489733ed356299a3b8e8035e
Administrador <ingalexisrojas2@gmail.com>
2024-03-21 10:56:56 -0500

    Commit inicial

---
+++ src/main/java/com/reservashoteles/ReservaDeHotelesApplication.java
@@ -0,0 +1,13 @@
+package com.reservashoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaDeHotelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaDeHotelesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reservashoteles/ReservaDeHotelesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reservashoteles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaDeHotelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
